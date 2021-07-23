8fd3351b9861a498c23846cc52246739bc29cec6
Mariam Khurshudyan <mariamarm98@gmail.com>
2021-07-23 22:35:24 +0400

    creating angular project using html/css template

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterLinkActive, RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './component/home/home.component';
+import { AboutComponent } from './component/about/about.component';
+import { ServiceComponent } from './component/service/service.component';
+import { NewsComponent } from './component/news/news.component';
+import { TrainerComponent } from './component/trainer/trainer.component';
+import { GalleryComponent } from './component/gallery/gallery.component';
+import { ContactComponent } from './component/contact/contact.component';
+import { HeaderComponent } from './component/header/header.component';
+import { FooterComponent } from './component/footer/footer.component';
+const routes: Routes = [
+  { path: '',   redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent} ,
+  { path: 'service', component: ServiceComponent },
+  { path: 'news', component: NewsComponent },
+  { path: 'trainer', component: TrainerComponent },
+  { path: 'gallery', component: GalleryComponent },
+  { path: 'contact', component: ContactComponent },
+  {path: 'header', component:HeaderComponent },
+  {path: 'footer', component: FooterComponent},
 
-const routes: Routes = [];
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './component/about/about.component';
+import { ServiceComponent } from './component/service/service.component';
+import { NewsComponent } from './component/news/news.component';
+import { TrainerComponent } from './component/trainer/trainer.component';
+import { GalleryComponent } from './component/gallery/gallery.component';
+import { ContactComponent } from './component/contact/contact.component';
+import { HomeComponent } from './component/home/home.component';
+import { HeaderComponent } from './component/header/header.component';
+import { FooterComponent } from './component/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ServiceComponent,
+    NewsComponent,
+    TrainerComponent,
+    GalleryComponent,
+    ContactComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
